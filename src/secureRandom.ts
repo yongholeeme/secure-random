b833d7a59594c88aed6223c551d294046cbfe41d
yongholeeme <yongholeeme@gmail.com>
2024-05-13 15:11:26 +0900

    ✨ Initial setup of core logic structure

--- src/secureRandom.ts
+++ src/secureRandom.ts
@@ -1,3 +1,27 @@
+function generateSecureRandom() {
+    try {
+        if (typeof window === 'undefined') {
+            /**
+             * @todo use Node API
+             * https://nodejs.org/api/crypto.html#cryptogetrandomvaluestypedarray
+             */
+
+            return Math.random()
+        } else {
+            /**
+             * @todo use Web API
+             * https://developer.mozilla.org/en-US/docs/Web/API/Crypto/getRandomValues
+             */
+
+            return Math.random()
+        }
+    } catch {
+        console.warn('Failed to generate secure random value, it returns Math.random()')
+    } finally {
+        return Math.random()
+    }
+}
+
 export function secureRandom(params: {min?: number; max?: number} = {}) {
     const {min = 0, max = 1} = params
 
@@ -5,5 +29,5 @@ export function secureRandom(params: {min?: number; max?: number} = {}) {
         throw new Error('InvalidParameterError')
     }
 
-    return Infinity
+    return generateSecureRandom() * (max - min) + min
 }
